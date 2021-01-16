96da39e2bd792af88124afd9d743ec3dee9202cd
Parvaiz Khan <parvaizkhan217@gmail.com>
2021-01-16 19:08:11 +0530

    update the Main screen to render bare minimum UI for chat composing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import Main from './Main';
+import Main from '@alpha/Main';
 
 export type AppRoutes = {
   Main: undefined;
-}
+};
 
 const Stack = createStackNavigator<AppRoutes>();
 
@@ -14,7 +14,11 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Main" component={Main} options={{ title: 'Alpha' }} />
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{title: 'alpha', headerTitleAlign: 'center'}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useCallback} from 'react';
+import {View, FlatList, StyleSheet} from 'react-native';
+
+import {Composer} from '@alpha/components';
 
 const Main = () => {
+  const renderItem = useCallback(() => <></>, []);
 
-    return (
-        <View style={s.container}>
-            <Text style={s.text}>{'Alpha'}</Text>
-        </View>
-
-    )
-}
+  return (
+    <View style={s.container}>
+      <FlatList
+        data={[]}
+        contentContainerStyle={{flexGrow: 1}}
+        keyboardShouldPersistTaps={'handled'}
+        {...{renderItem}}
+      />
+      <Composer />
+    </View>
+  );
+};
 
 const s = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    text: {
-        fontSize: 18,
-        color: 'tomato'
-    }
+  container: {
+    flex: 1,
+  },
 });
 
 export default Main;
